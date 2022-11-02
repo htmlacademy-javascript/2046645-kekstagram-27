38cab39f174c4e12324becf776dcfa1fee718e2c
Александр <Kabatov-alex@mail.ru>
2022-11-02 15:38:22 +0300

    Добавляет функцию полноразмерного режима просмотра фотографий

--- js/picture.js
+++ js/picture.js
@@ -1,4 +1,5 @@
 import {getPictures} from './data.js';
+import {showBigPicture} from './big-picture.js';
 
 const userPictures = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -7,11 +8,15 @@ const randomUserPictures = getPictures();
 
 const pictureFragment = document.createDocumentFragment();
 
-randomUserPictures.forEach(({url, comments, likes}) => {
+randomUserPictures.forEach((picture) => {
+  const {url, comments, likes} = picture;
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.addEventListener('click', () => {
+    showBigPicture(picture);
+  });
   pictureFragment.appendChild(pictureElement);
 });
 
